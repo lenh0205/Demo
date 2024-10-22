189e4a819151ced63c522e12f59ba3f64c2e1300
lenh0205 <lenhtranchi98@gmail.com>
2024-10-22 08:08:28 +0700

    first commit

---
+++ Main/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+using Main.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Main.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IBusinessServicesFactory<Item> _businessServices;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IBusinessServicesFactory<Item> businessServices)
+        {
+            _logger = logger;
+            _businessServices = businessServices;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        public IActionResult GetItem()
+        {
+            var result = _businessServices.GetBusinessService().;
+            return Ok(result);
+        }
+    }
+}
---
+++ Main/Services/ItemService.cs
@@ -0,0 +1,15 @@
+﻿namespace Main.Services
+{
+    public class ItemService : IBusinessService
+    {
+        public string GetOne()
+        {
+            return "hello";
+        }
+    }
+
+    public class Item
+    {
+        public int Id { get; set; }
+    }
+}
