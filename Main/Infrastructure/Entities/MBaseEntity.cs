83dcaf6b8fdb5e5638d02f7dbdcfff911077f135
vietinfo123 <lenhtranchi98@gmail.com>
2024-10-25 17:35:46 +0700

    update Mongo Base Entity

---
+++ Main/Infrastructure/Entities/MBaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Main.Infrastructure.Entities
+{
+    public class MBaseEntity
+    {
+        [BsonId] // point out "Id" is key in table
+        [BsonRepresentation(BsonType.ObjectId)] // "Id" datatype is "ObjectId"
+        public string Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
